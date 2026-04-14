888b850d1c023c1e9da08753356c8489252dfab8
NJW1995 <nohg@plus-ex.com>
2026-04-14 13:18:02 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
 
-// 이 설정이 있으면 모든 페이지가 로그인 없이는 접근 불가하게 됩니다.
+export default withAuth({
+  pages: {
+    signIn: "/api/auth/signin",
+  },
+})
+
 export const config = { 
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"] 
 }
